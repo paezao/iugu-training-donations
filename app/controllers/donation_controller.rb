4ba262697a28c1be96d2adbb3ade5d708a3a2dd4
Marcelo Paez Sequeira <paezao@gmail.com>
2015-03-17 17:45:12 -0300

    [ ! ] Added thank you page and error handling

--- app/controllers/donation_controller.rb
+++ app/controllers/donation_controller.rb
@@ -1,6 +1,5 @@
 class DonationController < ApplicationController
   def index
-    
   end
 
   def donate
@@ -13,10 +12,17 @@ class DonationController < ApplicationController
         {
           "description" => "Training",
           "quantity" => "1",
-          "price_cents" => "1000"
+          "price_cents" => "100"
         }
       ]
     })
+
+    if charge.success
+      redirect_to thank_you_url
+    else
+      flash[:danger] = "Erro processando pagamento!" 
+      redirect_to root_path
+    end
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'donation#index'
 
   post '/', to: 'donation#donate', as: 'donate'
+
+  get '/thank_you', to: 'donation#thank_you', as: 'thank_you'
 end
