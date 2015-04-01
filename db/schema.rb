bcda68eb06561b728f00ba68f403bf4c5b08c5f5
Marcelo Paez Sequeira <paezao@gmail.com>
2015-04-01 17:16:52 -0300

    [ ! ] Preparing for deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150324203551) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "donations", force: :cascade do |t|
     t.string   "invoice_id"
     t.integer  "amount_cents"
